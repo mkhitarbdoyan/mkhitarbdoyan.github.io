b4d0dd6c14ca85322ab85e2c13e761acfbb591eb
mkhitar <31042425+mkhitarbdoyan@users.noreply.github.com>
2021-06-25 12:20:57 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,80 @@
+// const num1 = +prompt("inpute num1")
+// const sign = prompt("nshan")
+// const num2 = +prompt("inpute num2")
+// let rezalt = 0
+
+
+// if ((typeof(num1) == "number")&&(typeof(num2)=="number")) {
+//     switch (sign) {
+//         case "+":
+//             rezalt = num1 + num2;
+//             break
+//         case "-":
+//             rezalt = num1 - num2;
+//             break;
+//         case "/":
+//             rezalt = num1 / num2;
+//             break;
+//         case "*":
+//             rezalt = num1 * num2;
+//             break;
+//         case 
+//         default:
+//             rezalt = "mutqagrel nshan"
+//     }
+//     alert( `${num1} ${sign} ${num2} = ${rezalt}` )
+// }
+// else {
+//     alert("mutqagrel miayn tiv")
+// }
+//////////////////////////////////////max
+
+// const num1 = +prompt("inpute num1")
+
+// const num2 = +prompt("inpute num2")
+// const num3 = +prompt("inpute num3")
+
+// if ((num1 > num2) && (num1 > num3)) {
+//     alert(num1)
+// } else if ((num2 > num1) && (num2 > num3)) {
+//     alert(num2)
+// } else {
+//     alert(num3)
+// }
+
+//////////////////////////////parzel tiv@ 2i astijan e te che
+
+// let num = 120
+// let arj=true
+
+// for (let i = 1; i <= num ; i++){
+    
+//     (2**i)===num ? arj=!arj: arj=!arj
+
+// }
+// alert(arj)
+
+
+// //////////////////////////////
+// let num = 5
+
+// // alert((num - 1) ** 2)
+// // alert((num +1) **2)
+
+
+// ///////////////////////
+// let num = 1
+
+// alert((num+7)**2)
+
+
+
+
+
+
+
+
+
+
+
+
